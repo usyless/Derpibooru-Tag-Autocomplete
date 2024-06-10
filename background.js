9b7b99751b6c125e08b8563ce7032bb7f6f8f2a6
usy <67288004+usyless@users.noreply.github.com>
2024-06-10 04:51:35 +0100

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-browser.runtime.onMessage.addListener(async (request) => {
-    if (request.type === "tags") {
-        const page = request['page'];
-        let data = await fetch(`https://derpibooru.org/api/v1/json/search/tags?q=*${request['query']}*${page === 1 ? "" : `&page=${page}`}`, {method: "GET"});
-        data = await data.json();
-        return data['tags'];
-    }
-});
