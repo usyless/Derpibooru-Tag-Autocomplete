f28d0f4c4a84dd0804df6b3feff026e5e2afc9a2
usy <67288004+usyless@users.noreply.github.com>
2024-06-10 17:23:19 +0100

    support more search operators
    better keypress handling

--- derpi_tag_autocomplete.js
+++ derpi_tag_autocomplete.js
@@ -1,23 +1,22 @@
 'use strict';
 
-if (typeof browser === "undefined") {
-    var browser = chrome;
-}
-
 (() => {
     function autocomplete(input, ac_list) {
-        let focus = 0, recievedPage = true, lastQuery = '', page = 1;
-        const createListItem = listItemTemplate();
+        let focus = 0, recievedPage = true, lastQuery = '', page = 1, timer;
+        const createListItem = listItemTemplate(),
+            keyMappings = {
+                40: () => changeActive(true),
+                38: () => changeActive(false),
+                9: () => document.querySelector('[class*="ac-active"]').click()
+            };
 
         function addToInput(tag) {
             input.value = input.value.substring(0, input.value.length - lastQuery.length) + tag;
         }
 
         function listItemTemplate() {
-            const list = document.createElement('li'),
-                outer_div = document.createElement('div'),
-                text_div = document.createElement('div'),
-                number_div = document.createElement('div');
+            const list = document.createElement('li'), outer_div = document.createElement('div'),
+                text_div = document.createElement('div'), number_div = document.createElement('div');
             text_div.classList.add('text-div');
             number_div.classList.add('number-div');
             outer_div.append(text_div, number_div);
@@ -25,10 +24,9 @@ if (typeof browser === "undefined") {
 
             return (aliased_tag, name, count) => {
                 number_div.innerText = simplifyNumber(count);
-                const newList = list.cloneNode(true),
+                const newList = list.cloneNode(true), index = name.indexOf(lastQuery),
                     new_text_div = newList.firstChild.firstChild,
-                    strong = document.createElement('strong'),
-                    index = name.indexOf(lastQuery);
+                    strong = document.createElement('strong');
                 strong.innerText = name.substring(index, index + lastQuery.length);
                 new_text_div.append(document.createTextNode(name.substring(0, index)), strong, document.createTextNode(name.substring(index + lastQuery.length)));
                 if (aliased_tag) new_text_div.appendChild(document.createTextNode(` → ${aliased_tag}`));
@@ -36,8 +34,19 @@ if (typeof browser === "undefined") {
             }
         }
 
-        function displayAutocompleteResults(data) {
-            if (data.length > 0) {
+        function addItemToAutocomplete(data) {
+            const ac_item = createListItem(data['aliased_tag'], data['name'], data['images']);
+            ac_item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                addToInput(data['name']);
+                closeList();
+                input.focus();
+            });
+            ac_list.appendChild(ac_item);
+        }
+
+        function displayAutocompleteResults(q, data) {
+            if (data && q === lastQuery && data.length > 0) {
                 recievedPage = true;
                 if (page === 1) closeList();
                 for (const i of data) addItemToAutocomplete(i);
@@ -45,70 +54,46 @@ if (typeof browser === "undefined") {
             }
         }
 
-        function addItemToAutocomplete(data) {
-            const ac_item = createListItem(data['aliased_tag'], data['name'], data['images']);
-            ac_item.addEventListener('click', (e) => {
-                e.stopPropagation();
-                addToInput(data['name'], lastQuery.length);
-                closeList();
-                input.focus();
-            });
-            ac_list.appendChild(ac_item);
-        }
-
-        {
-            let timer;
-            input.addEventListener('input', async () => {
-                if (timer) clearTimeout(timer);
-                let query = input.value;
-                const split = query.split(',');
-                query = split[Math.max(0, split.length - 1)].trim();
-                if (query !== lastQuery) {
-                    if (query.length <= 0) {
-                        closeList();
-                        lastQuery = '';
-                    }
-                    else timer = setTimeout(() => getResults(query.toLowerCase()), 250);
-                }
-            });
-        }
-
-        async function getResults(query) {
+        async function getResults(newQuery) {
             recievedPage = false;
-            if (query) {
-                lastQuery = query;
+            if (newQuery) {
                 focus = 0;
                 page = 1;
-            }
-            else ++page;
-            displayAutocompleteResults((await (await fetch(`https://derpibooru.org/api/v1/json/search/tags?q=*${lastQuery}*${page === 1 ? "" : `&page=${page}`}`, {method: "GET"})).json())['tags']);
+            } else ++page;
+            const currentSearch = {q: lastQuery};
+            displayAutocompleteResults(currentSearch.q, (await (await fetch(`https://derpibooru.org/api/v1/json/search/tags?q=*${currentSearch.q}*&page=${page}`, {method: "GET"})).json())['tags']);
         }
 
-        input.addEventListener('keydown', (e) => {
-            const items = ac_list.children;
-            if (e.keyCode === 40) { // down
-                e.preventDefault();
-                changeActive(items, 1);
-            } else if (e.keyCode === 38) { // up
-                e.preventDefault();
-                changeActive(items, -1);
-            } else if (e.keyCode === 9) { // tab
-                e.preventDefault();
-                items[focus].click();
+        input.addEventListener('input', () => {
+            clearTimeout(timer);
+            const query = cleanQuery(input.value);
+            if (query !== lastQuery) {
+                lastQuery = query;
+                if (query.length <= 0) closeList();
+                else timer = setTimeout(() => getResults(true), 250);
             }
         });
 
-        function changeActive(items, effect) {
-            const oldItem = items[focus];
+        function changeActive(down) {
+            const oldItem = document.querySelector('[class*="ac-active"]');
             oldItem.classList.remove('ac-active');
-            focus += effect;
 
-            if (!recievedPage && focus >= items.length) focus = 0;
-            else if (focus < 0) focus = items.length - 1;
-            const newItem = items[focus];
+            let newItem = down ? oldItem.nextElementSibling : oldItem.previousElementSibling;
+            if (!newItem) {
+                if (!recievedPage && down) newItem = ac_list.firstElementChild;
+                else if (!down) newItem = ac_list.lastElementChild;
+            }
             newItem.classList.add('ac-active');
             newItem.scrollIntoView({block: 'center'});
         }
+
+        input.addEventListener('keydown', (e) => {
+            const action = keyMappings[e.keyCode];
+            if (action) {
+                e.preventDefault();
+                action();
+            }
+        });
 
         function closeList() {
             const newList = ac_list.cloneNode();
@@ -127,25 +112,39 @@ if (typeof browser === "undefined") {
         document.addEventListener('click', closeList);
     }
 
+    function getCleanQuery() {
+        const searchOperators = [',', ' AND ', ' OR ', ' \\|\\| ', ' && '],
+            regex = new RegExp(searchOperators.join('|'), 'g');
+
+        return (query) => {
+            query = query.trim().split(regex);
+            query = query[query.length - 1].trim();
+            if (query.length > 0) {
+                if (query[0] === '-') query = query.substring(1);
+                else if (query.substring(0, 4) === 'NOT ') query = query.substring(4);
+            }
+            return query.toLowerCase();
+        }
+    }
+
+    const cleanQuery = getCleanQuery();
+
     function simplifyNumber(number) {
         if (number < 1000) return number.toString();
         else if (number < 1000000) return `${(number / 1000).toFixed(1)}K`;
         else return `${(number / 1000000).toFixed(1)}M`;
     }
 
-    const form = document.getElementById('q').parentElement,
-        div = document.createElement('div');
+    const input = document.getElementById('q'), form = input.parentElement, div = document.createElement('div'),
+        ac_list = document.createElement('div');
     if (form) {
         div.classList.add('ac', ...form.classList);
         form.before(div);
         div.appendChild(form);
 
-        const input = document.getElementById('q');
         input.autocomplete = 'off';
 
-        const ac_list = document.createElement('div');
-        ac_list.id = 'ac-list';
-        ac_list.classList.add('ac-items');
+        ac_list.classList.add('ac-list');
         input.parentNode.parentNode.appendChild(ac_list);
 
         autocomplete(input, ac_list);
