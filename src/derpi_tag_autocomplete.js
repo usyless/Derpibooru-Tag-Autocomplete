6b4ffe3eba8ff5d456700214a51d68eabde9bf98
usy <contact.usyless@gmail.com>
2025-01-25 15:40:25 +0000

    add reject reason

--- src/derpi_tag_autocomplete.js
+++ src/derpi_tag_autocomplete.js
@@ -225,7 +225,7 @@
                             if (controller.signal.aborted) reject('Autocomplete Cancelled');
                             else resolve(r);
                         });
-                    } else reject();
+                    } else reject('Autocomplete loading, please wait.');
                 });
             });
         } else {
